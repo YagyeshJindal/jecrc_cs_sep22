caf02c2509319340d036cd083ba5c09ac0b6a940
Yagyesh Jindal <yagyeshjindal.cse25@jecrc.ac.in>
2022-10-10 15:37:31 +0530

    day16

--- day12/recommender.py
+++ day12/recommender.py
@@ -1,12 +1,24 @@
 import tkinter as tkt
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
 
 app = tkt.Tk()
 
 app.title('recommendation system')
 app.geometry('400x400')
 
+cols=['user_id','movie_id','rating','ts']
+df = pd.read_csv('u.data',sep = '\t',names=cols).drop('ts',axis=1)
+item_cols=['movie_id','title']+[str(i) for i in range(22)]
+df1=pd.read_csv('u.item',sep='|',names=item_cols,encoding = "ISO-8859-1")[['movie_id','title']]
+movie = pd.merge(df,df1,on='movie_id')
+
 result = tkt.Varaible(app)
 box = tkt.Listbox(app, height=10)
+for row, val in movie.iterrows():
+    print(val['title'])
+    box.insert(row+1, val['title'])
 box.place(x=10,y=10)
 
 def get_movie():
